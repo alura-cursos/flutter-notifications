f3f54402bed2cbe4b5eeed3905377ed3d01092f1
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-08-10 15:29:41 -0300

    Updates do code, ainda quebrado

--- lib/http/web.dart
+++ lib/http/web.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'package:dev_meetups/models/device.dart';
 import 'package:dev_meetups/models/event.dart';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = 'http://192.168.100.76:8080/api';
 
+// Busca todos os eventos cadastrados
 Future<List<Event>> getAllEvents() async {
   final response = await http
       .get(Uri.parse('$baseUrl/events'));
@@ -15,6 +17,27 @@ Future<List<Event>> getAllEvents() async {
   else {
     throw Exception('Falha ao carregar os eventos');
   }
+}
 
+// Cadastra um novo token de dispositivo na aplicação web
+Future<Device> createDevice(String token) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/devices'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'token': token,
+    }),
+  );
 
+  if (response.statusCode == 201) {
+    // Se o servidor retornou uma resposta 201 CREATED,
+    // em seguida, converte o JSON.
+    return Device.fromJson(jsonDecode(response.body));
+  } else {
+    // Se o servidor não retornou uma resposta 201 CREATED,
+    // então lança uma exceção.
+    throw Exception('Failed to create album.');
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:dev_meetups/screens/events_screen.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  String? token = await messaging.getToken();
+  print('================= TOKEN: $token');
+
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dev meetups',
